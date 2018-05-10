2c81b1727af56427969ac1d241feb6a4763a52ec
Daniel Svane <ds@nanolink.com>
2018-05-10 15:53:41 +0200

    Init

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="getDevices">Lol</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      getDevices () {
+        console.log('lol')
+        navigator.bluetooth.requestDevice({"acceptAllDevices":true})
+        .then(device => {
+          log('> Name:             ' + device.name);
+          log('> Id:               ' + device.id);
+          log('> Connected:        ' + device.gatt.connected);
+        })
+      }
+    },
+    mounted () {
+
+    }
+  }
+</script>
+
+<style>
+</style>
